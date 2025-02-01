6e26a80d00d54afc9ba09f096986421efd5c4ea9
jahaziel127g <jahazielolivera127127@gmail.com>
2025-02-01 09:50:27 +0400

    Update task.c

--- task.c
+++ task.c
@@ -1,8 +1,13 @@
 #include "task.h"
 #include "memory.h"
+#include "isr.h"
+#include "ports.h"
 
 #define MAX_TASKS 4
 #define STACK_SIZE 1024
+#define PIT_COMMAND 0x43
+#define PIT_DATA 0x40
+#define PIT_FREQUENCY 100  // 100 Hz
 
 typedef struct {
     void (*entry)();
@@ -13,10 +18,34 @@ typedef struct {
 static Task tasks[MAX_TASKS];
 static int current_task = 0;
 
+void switch_task() {
+    for (int i = 0; i < MAX_TASKS; i++) {
+        current_task = (current_task + 1) % MAX_TASKS;
+        if (tasks[current_task].active) {
+            tasks[current_task].entry();
+            return;
+        }
+    }
+}
+
+void pit_callback() {
+    switch_task();
+}
+
+void init_pit() {
+    int divisor = 1193180 / PIT_FREQUENCY;
+    port_byte_out(PIT_COMMAND, 0x36);
+    port_byte_out(PIT_DATA, divisor & 0xFF);
+    port_byte_out(PIT_DATA, (divisor >> 8) & 0xFF);
+    
+    register_interrupt_handler(32, pit_callback);  // IRQ0 = Timer
+}
+
 void task_init() {
     for (int i = 0; i < MAX_TASKS; i++) {
         tasks[i].active = 0;
     }
+    init_pit();
 }
 
 int task_create(void (*entry)()) {
@@ -28,14 +57,4 @@ int task_create(void (*entry)()) {
         }
     }
     return -1;
-}
-
-void task_switch() {
-    for (int i = 0; i < MAX_TASKS; i++) {
-        current_task = (current_task + 1) % MAX_TASKS;
-        if (tasks[current_task].active) {
-            tasks[current_task].entry();
-            return;
-        }
-    }
 }
