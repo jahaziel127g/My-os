21cafbef415d9ada3055a806ed16614f3f240eec
jahaziel127g <jahazielolivera127127@gmail.com>
2025-02-03 13:19:29 +0400

    Update memory.h

--- memory.h
+++ memory.h
@@ -1,6 +1,10 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+typedef struct PageDirectory PageDirectory;
+
 void setup_paging();
+PageDirectory* create_page_directory();
+void load_page_directory(PageDirectory* dir);
 
 #endif
