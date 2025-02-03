4e809691a74573412c5a2545123bdaa9074c24da
jahaziel127g <jahazielolivera127127@gmail.com>
2025-02-03 13:20:02 +0400

    Update task.c

--- task.c
+++ task.c
@@ -1,60 +1,47 @@
 #include "task.h"
 #include "memory.h"
-#include "isr.h"
-#include "ports.h"
 
 #define MAX_TASKS 4
 #define STACK_SIZE 1024
-#define PIT_COMMAND 0x43
-#define PIT_DATA 0x40
-#define PIT_FREQUENCY 100  // 100 Hz
 
 typedef struct {
     void (*entry)();
     char stack[STACK_SIZE];
     int active;
+    PageDirectory* page_directory;
 } Task;
 
 static Task tasks[MAX_TASKS];
 static int current_task = 0;
 
-void switch_task() {
-    for (int i = 0; i < MAX_TASKS; i++) {
-        current_task = (current_task + 1) % MAX_TASKS;
-        if (tasks[current_task].active) {
-            tasks[current_task].entry();
-            return;
-        }
-    }
-}
-
-void pit_callback() {
-    switch_task();
-}
-
-void init_pit() {
-    int divisor = 1193180 / PIT_FREQUENCY;
-    port_byte_out(PIT_COMMAND, 0x36);
-    port_byte_out(PIT_DATA, divisor & 0xFF);
-    port_byte_out(PIT_DATA, (divisor >> 8) & 0xFF);
-    
-    register_interrupt_handler(32, pit_callback);  // IRQ0 = Timer
-}
-
 void task_init() {
     for (int i = 0; i < MAX_TASKS; i++) {
         tasks[i].active = 0;
     }
-    init_pit();
 }
 
-int task_create(void (*entry)()) {
+int task_create(void (*entry)(), int user_mode) {
     for (int i = 0; i < MAX_TASKS; i++) {
         if (!tasks[i].active) {
             tasks[i].entry = entry;
             tasks[i].active = 1;
+            tasks[i].page_directory = create_page_directory();
+            if (user_mode) {
+                tasks[i].page_directory->physical_tables[1023] |= 0x4; // Mark as user mode
+            }
             return i;
         }
     }
     return -1;
+}
+
+void task_switch() {
+    for (int i = 0; i < MAX_TASKS; i++) {
+        current_task = (current_task + 1) % MAX_TASKS;
+        if (tasks[current_task].active) {
+            load_page_directory(tasks[current_task].page_directory);
+            tasks[current_task].entry();
+            return;
+        }
+    }
 }
